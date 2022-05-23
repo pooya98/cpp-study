40a247726b893b696362989f27286feb4e25db3c
pooya98 <pooya98@naver.com>
2022-05-23 19:09:48 +0900

    Chap01

---
+++ Chap01/Q01_04/header.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "header.h"
+
+void BestComImpl::SimpleFunc(void)
+{
+    std::cout<<"BestCom이 정의한 함수"<<std::endl;
+}
+
+void ProgComImpl::SimpleFunc(void)
+{
+    std::cout<<"ProgCom이 정의한 함수"<<std::endl;
+}
+
